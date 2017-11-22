37580f89e01f9abcdca39502400434ddda13ff84
Andrey Kovalev <andreyykovalev@gmail.com>
2017-11-22 13:07:18 +0200

    second commit

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-var resetButton = document.getElementById("reset-button");
-
-var colors = [];
-for (var i = 0; i < 10; i++) {
-  colors.push('square-' + i);
-}
-
-function GameSquare(el, color) {
-  this.el = el;
-  this.isOpen = false;
-  this.isLocked = false;
-  this.el.addEventListener("click", this, false);
-}
-
+alert(1);
